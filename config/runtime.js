278d7ec36fb11c02784436aab43fe5c8d07bce0c
chmsantos <30699695+chmsantos@users.noreply.github.com>
2017-08-04 02:56:49 +0800

    Update runtime.js

--- config/runtime.js
+++ config/runtime.js
@@ -2,8 +2,8 @@
 "twitter" : {
     "consumer_key" : "pX0fKRGZm9PCLYFfypzGqKapT",
     "consumer_secret" : "Mz8cQ30FkFwJgfx9UY3gubHJAN5T9l0L3beLPtnTq5t25e9M2f",
-    "access_token_key" : "	893162196611375104-9ateNK2G7j8OayJ26VYjwn0JX4KMCXT",
-    "access_token_secret" : "	eIeWXxWfOgHbedj1u7215zlpyqlAzHfQ2zCCQP0uTFxRj"
+    "access_token_key" : "893162196611375104-znKnijV1MUWim9X4b78reAVSW6aJPhY",
+    "access_token_secret" : "ox714yQxh9ysFsb5SdxtfRpIot1ABY9UGunGT00YT6DTO"
   },
   "keywords" : "need hug",
   "match" : "match" : "a string or regex. (capture groups are allowed)",
